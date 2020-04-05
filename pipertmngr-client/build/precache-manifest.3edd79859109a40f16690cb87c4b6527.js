77cb2f619253870b37167618ce21226d8fcecfbe
tomer <tomerdayan168@gmail.com>
2020-04-05 16:01:20 +0300

    replace the design of select file

---
+++ pipertmngr-client/build/precache-manifest.3edd79859109a40f16690cb87c4b6527.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792404ea7de5dfa783dc0f379895d5ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd8c976371d8e0d962ff",
+    "url": "/static/css/2.2089bce0.chunk.css"
+  },
+  {
+    "revision": "103db9981dee3739749b",
+    "url": "/static/css/main.8e18765d.chunk.css"
+  },
+  {
+    "revision": "cd8c976371d8e0d962ff",
+    "url": "/static/js/2.037fe06e.chunk.js"
+  },
+  {
+    "revision": "4ab85d8c00ac0e4a6b9f0e03c7b60b5e",
+    "url": "/static/js/2.037fe06e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103db9981dee3739749b",
+    "url": "/static/js/main.8a4f01d8.chunk.js"
+  },
+  {
+    "revision": "8a382bf81e03bd4cdc10",
+    "url": "/static/js/runtime-main.1eea3ea3.js"
+  },
+  {
+    "revision": "27a6a1f1070e624518060e9dc8fa9e53",
+    "url": "/static/media/OpenSansHebrew-Bold.27a6a1f1.ttf"
+  },
+  {
+    "revision": "11544c36c3b76127e8d84e894b4f1230",
+    "url": "/static/media/OpenSansHebrew-Regular.11544c36.ttf"
+  },
+  {
+    "revision": "36b5bab58a18b9c924861a4ccbf1a790",
+    "url": "/static/media/Roboto-Bold.36b5bab5.ttf"
+  },
+  {
+    "revision": "5673da52c98bb6cb33ada5aaf649703e",
+    "url": "/static/media/Roboto-Regular.5673da52.ttf"
+  },
+  {
+    "revision": "14147ca3288fa08b850d029193150722",
+    "url": "/static/media/Roboto-Thin.14147ca3.ttf"
+  }
+]);
